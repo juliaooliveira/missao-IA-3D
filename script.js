a6ef7e05f1f3da8737c01be531dfa27231cf3459
juliaooliveira <148073511+juliaooliveira@users.noreply.github.com>
2024-09-11 10:21:35 -0300

    Update script.js

--- script.js
+++ script.js
@@ -4,67 +4,6 @@ const caixaAlternativas = document.querySelector(".caixa-alternativas");
 const caixaResultado = document.querySelector(".caixa-resultado");
 const textoResultado = document.querySelector(".texto-resultado");
 
-const perguntas = [
-    {
-        enunciado: "Qual dos animais marinhos é um mamífero?",
-        alternativas: [
-            {
-                texto: "Baleia",
-                afirmacao: [
-                    "Se essa foi a opçâo escolhida, está correto.",
-                    "afirmacao 2"
-                    ]
-            },
-            {
-                texto: "Cavalo marinho",
-                afirmacao: [
-                    "Se essa foi a opçâo escolhida, está incorreta.",
-                    "afirmacao 2"
-                    ]
-            }           
-            
-        ]
-    },
-    {
-        enunciado: "Qual é o único mamífero que voa?",
-        alternativas: [
-            {
-                texto:"Morcego",
-                afirmacao: [
-                    "afirmacao 1",
-                    "afirmacao 2"
-                    ]
-            },
-            {
-                texto: "Alternativa 2 da pergunta 2",
-                afirmacao: [
-                    "afirmacao 1",
-                    "afirmacao 2"
-                    ]
-            }
-        ]
-    },
-    {
-        enunciado: "Qual é o mamífero que bota ovo?",
-        alternativas: [
-            {
-                texto:"Alternativa 1 da pergunta 3",
-                afirmacao: [
-                    "afirmacao 1",
-                    "afirmacao 2"
-                    ]
-            },
-            {
-                texto:"Alternativa 2 da pergunta 3",
-                afirmacao: [
-                    "afirmacao 1",
-                    "afirmacao 2"
-                    ]
-            }
-            
-        ]
-    },
-];
 
 let atual = 0; 
 let perguntaAtual;
@@ -98,7 +37,7 @@ function respostaSelecionada(opcaoSelecionada){
 }
 
 function mostraResultado(){
-    caixaPerguntas.textContent = "Em 2049...";
+    caixaPerguntas.textContent = `Em 2049, $(nome)`;
     textoResultado.textContent = historiaFinal;
     caixaAlternativas.textContent = ""; 
     mostraPergunta();
