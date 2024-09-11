54cdabce79fa2c44d29c7308148a288456fcd28c
juliaooliveira <148073511+juliaooliveira@users.noreply.github.com>
2024-09-11 10:15:58 -0300

    Update aleatorio.js

--- js/aleatorio.js
+++ js/aleatorio.js
@@ -1,1 +1,6 @@
+const nomes = ["Jung-kook", "Alana Delarey", "Ye", "kim jong un", "Tulla", "Ygona"
 
+
+export function aleatorio (lista){
+  const posicao = Math.floor(Math.random()* lista.lenght);
+  return lista[posicao];
